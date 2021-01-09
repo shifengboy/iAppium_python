38fec65084210bec9e336c6b0a61f006205f65d9
chenshifeng <1162704960@qq.com>
2021-01-09 15:21:40 +0800

    注释掉udid

--- test/cm_test.py
+++ test/cm_test.py
@@ -14,7 +14,7 @@ class IAppium(unittest.TestCase):
         desired_caps = {}
         appium_server_url = config['appium_server_url']
         desired_caps['platformName'] = config['desired_caps']['platformName']
-        desired_caps['udid'] = config['desired_caps']['udid']
+        # desired_caps['udid'] = config['desired_caps']['udid']
         desired_caps['deviceName'] = config['desired_caps']['deviceName']
         desired_caps['appPackage'] = config['desired_caps']['appPackage']
         desired_caps['appActivity'] = config['desired_caps']['appActivity']
